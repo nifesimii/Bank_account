404aed1eb2b6091199a481c98f813be568a0d9eb
Oluwanifesimi Ademoye <nifesimifrank@gmail.com>
2021-05-12 00:58:47 +0100

    docs: Uploading mages to readme

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptoools import setup
+
+setup(name='Bank-Account'
+	version='1.2',
+	description='Bank Account representatio using Object Oriented Programming',
+	packages=['Bank-Account'],
+	author = 'Nifesimi Ademoye',
+	author_email = 'nifesimifrank@gmail.com',
+	zip_safe=False)

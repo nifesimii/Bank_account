a6c8a2878590e090d3f4ed00df1383796ee5b0b4
Oluwanifesimi Ademoye <nifesimifrank@gmail.com>
2021-05-13 15:12:35 +0100

    Feat: Added rest of the programme after uploading to pypI

---
+++ Bank_Cli/README.md
@@ -0,0 +1,10 @@
+# Bank package
+
+Summary of the package 
+Thia is a Bank package  in command line intepretation
+#Files
+It contains a Gaussian distribution file, General distribution file, Binomial distribution file 
+
+Explanation of files in the package 
+You can calculate the mean, standard deviation of any distribution for both Gaussian and Binomial distribution 
+# Installation 
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptoools import setup
+from setuptools import setup
 
-setup(name='Bank-Account'
-	version='1.2',
-	description='Bank Account representatio using Object Oriented Programming',
-	packages=['Bank-Account'],
+setup(name='Bank_Account_Cli',
+	version = '1.2',
+	description='Bank Account representation using Object Oriented Programming',
+	packages=['Bank_Account_Cli'],
 	author = 'Nifesimi Ademoye',
-	author_email = 'nifesimifrank@gmail.com',
+	author_email= 'nifesimifrank@gmail.com',
 	zip_safe=False)
